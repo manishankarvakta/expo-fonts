354b37751d5dbfea38c669b7cc1093b7c7fa20d5
Atiqule Islam <manishankarvakta@egmail.com>
2023-04-18 23:42:46 +0600

    expo fonts tutorial

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [loadFont] = useFonts({
+    ador: require("./assets/fonts/LiAdorNoirritBold.ttf"),
+    luxS: require("./assets/fonts/LuxuriousScript-Regular.ttf"),
+    nova: require("./assets/fonts/NovaOval-Regular.ttf"),
+  })
+  
+  if (!loadFont) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={[styles.text, {fontFamily:"aror"} ]}>আদর নৈর্ঋত</Text>
+      <Text style={[styles.text,  {fontFamily:"luxS"}]}>Nova Oval</Text>
+      <Text style={[styles.text,  {fontFamily:"nova"}]}>Luxurious Script</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +29,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize:40,
+    marginBottom:10
+  }
 });
